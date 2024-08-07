1ee799f248bd35f81210eebfa82addcdfd12786b
ricrudo <66228463+ricrudo@users.noreply.github.com>
2024-08-07 10:34:51 -0500

    Update system_card_A.vue
    4_6 Andrea

--- src/components/section4/Q_4_6/system_card_A.vue
+++ src/components/section4/Q_4_6/system_card_A.vue
@@ -2,6 +2,9 @@
 
   import { ref } from 'vue'
   import CardHeader from '@comp/elements/cardHeader.vue'
+  import InputText from '@comp/elements/inputText.vue'
+    import DateInput from '@comp/elements/dateInput.vue'
+    import MultipleInputText from '@comp/elements/multipleInputText.vue'
   const show = ref(true)
 
   function handleToggle() {
@@ -17,14 +20,56 @@
 
       <CardHeader
           :title="[
-                  'Proyecto 1'
+                  'Libro o capítulo 1'
           ]"
           @click="handleToggle"
           />
 
       <div class="card-content toggle_show" v-show="show">
 
+          <InputText
+            id_name="titulo"
+            label="Título del libro y/o capitulo"
+            content=""
+            add_info= ""
+            :isRequired="true"
+            :stackClass="true"
+          />
 
+          <DateInput
+            id_name="fecha"
+            label="Fecha de publicación"
+            value=""
+            add_info=""
+            :isRequired="true"
+            :stackClass="true"
+          />
+
+          <MultipleInputText
+            id_name="autores"
+            textLabel="Lista los nombres de los autores y autoras con el botón &quot;Añadir&quot;"
+            textAddButton="+ Añadir autor/a"
+            :isRequired="true"
+          />
+
+
+          <InputText
+            id_name="editorial"
+            label="Editorial"
+            content=""
+            add_info= ""
+            :isRequired="true"
+            :stackClass="true"
+          />
+
+          <InputText
+            id_name="issn"
+            label="ISSN (International Standard Serial Number)"
+            content=""
+            add_info= ". Ej.: 1234-5678"
+            :isRequired="true"
+            :stackClass="true"
+          />
 
 
 
@@ -38,3 +83,4 @@
   width: 100%;
 }
 </style>
+
