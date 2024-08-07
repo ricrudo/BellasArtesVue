b9c2796393a652eb597b4f7cbf0706e0a24f1233
ricrudo <66228463+ricrudo@users.noreply.github.com>
2024-08-07 10:23:26 -0500

    Update system_card_A.vue
    Andrea

--- src/components/section4/Q_4_1/system_card_A.vue
+++ src/components/section4/Q_4_1/system_card_A.vue
@@ -2,6 +2,11 @@
 
   import { ref } from 'vue'
   import CardHeader from '@comp/elements/cardHeader.vue'
+    import DropdownList from '@comp/elements/dropdownList.vue'
+    import InputText from '@comp/elements/inputText.vue'
+    import LongText from '@comp/elements/textoAmplio.vue'
+    import CheckBoxMultiple from '@comp/elements/checkboxMultipleOption.vue'
+    
   const show = ref(true)
 
   function handleToggle() {
@@ -17,12 +22,65 @@
 
       <CardHeader
           :title="[
-                  'Proyecto 1'
+                  'Grupo de investigación 1'
           ]"
           @click="handleToggle"
           />
 
       <div class="card-content toggle_show" v-show="show">
+
+      <DropdownList
+        id_name="grupo"
+        label="Nombre del grupo"
+        :options="[
+          {'arte-accion': 'ARTE – ACCIÓN'},
+          {'sam': 'Sapiencia, Arte y Música (S.A.M.)'},
+          {'feliza': 'Feliza Bursztyn: Redes, Arte, Cultura['},
+          {'videns': 'Investigaciones Visuales del Caribe VIDENS'},
+          {'tei': 'Teatro, Espacio e Interactividad TEI'}
+          ]"
+        add_info= ""
+        :stackClass="true"
+      />
+
+      <DropdownList
+        id_name="programa"
+        label="Programa"
+        :options="[
+          {'licenciatura_en_musica': 'Licenciatura en música'},
+          {'musica': 'Música'},
+          {'arte_dramatico': 'Arte dramático'},
+          {'danza': 'Danza'},
+          {'artes_plasticas': 'Artes Plásticas'}
+          ]"
+        add_info= ""
+        :stackClass="true"
+      />
+
+      <CheckBoxMultiple
+        id_name="linea_investiga"
+        label="Línea de investigación"
+        :options="[
+                 {id_name:'musical', text:'Creación musical'},
+                 {id_name:'cultura', text:'Desarrollo y cultura'},
+                 {id_name:'estetica', text:'Estética'},
+                 {id_name:'pedagogia', text:'Pedagogía'},
+        ]"
+        add_info=""
+        :isRequired="true"
+        :stackClass="false"
+      />
+
+
+        <LongText
+          id_name="actividades"
+          label="Actividades"
+          content=""
+          add_info="Breve descripción de las actividades llevadas a cabo"
+          :isRequired="true"
+          :stackClass="false"
+        />
+
 
 
 
@@ -38,3 +96,4 @@
   width: 100%;
 }
 </style>
+
