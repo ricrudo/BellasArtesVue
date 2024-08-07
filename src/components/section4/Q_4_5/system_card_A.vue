3c256af4b6c0afdc9926eee2b65fe9604562070d
ricrudo <66228463+ricrudo@users.noreply.github.com>
2024-08-07 10:33:22 -0500

    Update system_card_A.vue
    4_5 Andrea

--- src/components/section4/Q_4_5/system_card_A.vue
+++ src/components/section4/Q_4_5/system_card_A.vue
@@ -2,6 +2,10 @@
 
   import { ref } from 'vue'
   import CardHeader from '@comp/elements/cardHeader.vue'
+  import InputText from '@comp/elements/inputText.vue'
+    import DropdownList from '@comp/elements/dropdownList.vue'
+    import DateInput from '@comp/elements/dateInput.vue'
+    import MultipleInputText from '@comp/elements/multipleInputText.vue'
   const show = ref(true)
 
   function handleToggle() {
@@ -17,12 +21,82 @@
 
       <CardHeader
           :title="[
-                  'Proyecto 1'
+                  'Artículo 1'
           ]"
           @click="handleToggle"
           />
 
       <div class="card-content toggle_show" v-show="show">
+
+      
+
+      <InputText
+        id_name="titulo"
+        label="Título del artículo"
+        content=""
+        add_info= ""
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+      <DateInput
+        id_name="fecha"
+        label="Fecha"
+        value=""
+        add_info=""
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+
+      <MultipleInputText
+        id_name="autores"
+        textLabel="Lista los nombres de los autores y autoras con el botón &quot;Añadir&quot;"
+        textAddButton="+ Añadir autor/a"
+        :isRequired="true"
+      />
+      
+
+      <InputText
+        id_name="issn"
+        label="ISSN (International Standard Serial Number)"
+        content=""
+        add_info= ". Ej.: 1234-5678"
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+      <InputText
+        id_name="link"
+        label="Link de acceso"
+        content=""
+        add_info= ""
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+      <InputText
+        id_name="revista"
+        label="Nombre de la revista"
+        content=""
+        add_info= ""
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+      <DropdownList
+        id_name="index_revista"
+        label="Tipo de indexación de la revista"
+        :options="[
+          {'publindex': 'Publindex'},
+          {'wos': 'WOS'},
+          {'scopus': 'Scopus'},
+          {'otros': 'Otros'},
+          ]"
+        add_info= ""
+        :isRequired="true"
+        :stackClass="true"
+      />
 
 
 
