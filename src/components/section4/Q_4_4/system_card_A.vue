5e11b080ffe13a545bd09f9a92af4789d39cfee0
ricrudo <66228463+ricrudo@users.noreply.github.com>
2024-08-07 10:29:43 -0500

    Update system_card_A.vue
    Andrea

--- src/components/section4/Q_4_4/system_card_A.vue
+++ src/components/section4/Q_4_4/system_card_A.vue
@@ -2,6 +2,9 @@
 
   import { ref } from 'vue'
   import CardHeader from '@comp/elements/cardHeader.vue'
+    import InputText from '@comp/elements/inputText.vue'
+    import DropdownList from '@comp/elements/dropdownList.vue'
+    import DateInput from '@comp/elements/dateInput.vue'
   const show = ref(true)
 
   function handleToggle() {
@@ -17,12 +20,89 @@
 
       <CardHeader
           :title="[
-                  'Proyecto 1'
+                  'Producto 1'
           ]"
           @click="handleToggle"
           />
 
       <div class="card-content toggle_show" v-show="show">
+
+      <DropdownList
+        id_name="tipo_producto"
+        label="Tipo de producto"
+        :options="[
+          {'gnc': 'Generación de nuevo conocimiento'},
+          {'dti': 'Desarrollo tecnológico e innovación'},
+          {'asc': 'Apropiación social del conocimiento'},
+          {'frh': 'Formación de Recurso Humano en CTeI'},
+          ]"
+        add_info= "Características de la actividad de la cual el producto es resultado"
+        :stackClass="false"
+      />
+
+
+
+      <InputText
+        id_name="nombre_producto"
+        label="Nombre del producto"
+        content=""
+        add_info= ""
+        :stackClass="true"
+      />
+
+      <InputText
+        id_name="institucion"
+        label="Institución"
+        content=""
+        add_info= "Institución donde fue exhibido o presentado"
+        :stackClass="true"
+      />
+
+      <DropdownList
+        id_name="alcance"
+        label="Alcance"
+        :options="[
+          {'local': 'Local'},
+          {'regional': 'Regional'},
+          {'nacional': 'Nacional'},
+          {'internacional': 'Internacional'},
+          ]"
+        add_info= ""
+        :stackClass="true"
+      />
+
+
+
+      <DateInput
+        id_name="fecha"
+        label="Fecha"
+        value=""
+        add_info=""
+        :isRequired="true"
+        :stackClass="true"
+      />
+
+      <InputText
+        id_name="institucion"
+        label="Link de difusión o nombre del catálogo"
+        content=""
+        add_info= "Link de difusión del evento/obra o nombre del catálogo si lo hubiera"
+        :stackClass="true"
+      />
+
+      
+
+      <DropdownList
+        id_name="categoria"
+        label="Categoría de reconocimiento"
+        :options="[
+          {'tipo_a': 'Tipo A'},
+          {'tipo_b': 'Tipo B'},
+          {'no_responde': 'No ha sido reconocido'},
+          ]"
+        add_info= ""
+        :stackClass="true"
+      />
 
 
 
@@ -32,6 +112,12 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.card-content {
+  width: 100%;
+}
+</style>
 
 <style scoped>
 .card-content {
